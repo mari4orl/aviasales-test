0f11efd199647f2332f6878729faf6bda8452a73
mari4orl <mari4orl@mail.ru>
2022-03-22 20:40:23 +0300

    Добавляет сортировку

--- build/js/main.js
+++ build/js/main.js
@@ -67,7 +67,7 @@ const insertData = (data) => {
   const template = data.map((ticket) => {
     return createTicket(ticket)
   }).join('')
-  container.insertAdjacentHTML("afterbegin", template);
+  container.innerHTML = template;
 }
 
 const URL = 'https://front-test.beta.aviasales.ru/search';
@@ -85,140 +85,16 @@ const request = (URL) => {
   }).catch(onError)
 }
 
-// Начало
-const arrTest = [
-    {
-      "price": 15529,
-      "carrier": "FV",
-      "segments": [
-        {
-          "origin": "MOW",
-          "destination": "HKT",
-          "date": "2021-10-22T09:59:00.000Z",
-          "stops": [
-            "HKG"
-          ],
-          "duration": 1072
-        },
-        {
-          "origin": "HKT",
-          "destination": "MOW",
-          "date": "2021-11-11T07:10:00.000Z",
-          "stops": [
-            "HKG"
-          ],
-          "duration": 1190
-        }
-      ]
-    },
-    {
-      "price": 42564,
-      "carrier": "FV",
-      "segments": [
-        {
-          "origin": "MOW",
-          "destination": "HKT",
-          "date": "2021-10-22T09:37:00.000Z",
-          "stops": [
-            "SIN"
-          ],
-          "duration": 702
-        },
-        {
-          "origin": "HKT",
-          "destination": "MOW",
-          "date": "2021-11-11T03:53:00.000Z",
-          "stops": [
-            "IST",
-            "SHA"
-          ],
-          "duration": 1008
-        }
-      ]
-    },
-    {
-      "price": 72842,
-      "carrier": "MH",
-      "segments": [
-        {
-          "origin": "MOW",
-          "destination": "HKT",
-          "date": "2021-10-22T12:01:00.000Z",
-          "stops": [
-            "HKG",
-            "SHA"
-          ],
-          "duration": 1217
-        },
-        {
-          "origin": "HKT",
-          "destination": "MOW",
-          "date": "2021-11-11T04:25:00.000Z",
-          "stops": [],
-          "duration": 1702
-        }
-      ]
-    },
-    {
-      "price": 45781,
-      "carrier": "SU",
-      "segments": [
-        {
-          "origin": "MOW",
-          "destination": "HKT",
-          "date": "2021-10-22T15:30:00.000Z",
-          "stops": [],
-          "duration": 1421
-        },
-        {
-          "origin": "HKT",
-          "destination": "MOW",
-          "date": "2021-11-11T05:27:00.000Z",
-          "stops": [],
-          "duration": 1670
-        }
-      ]
-    },
-    {
-      "price": 98845,
-      "carrier": "MH",
-      "segments": [
-        {
-          "origin": "MOW",
-          "destination": "HKT",
-          "date": "2021-10-22T00:58:00.000Z",
-          "stops": [
-            "SIN",
-            "BKK"
-          ],
-          "duration": 716
-        },
-        {
-          "origin": "HKT",
-          "destination": "MOW",
-          "date": "2021-11-10T23:44:00.000Z",
-          "stops": [
-            "IST"
-          ],
-          "duration": 1844
-        }
-      ]
-    }
-];
 const filters = document.querySelectorAll('.filters__checkbox');
 
-const filtersInit = (ticketsArr, element) => {
+const filtersInit = (ticketsArr, filterElem) => {
   return ticketsArr.filter((ticket) => {
-    console.log('filter ', element.id);
-    console.log('ticket ', ticket.segments[0].stops.length);
-    return parseInt(element.id) === ticket.segments[0].stops.length
+    const filter = parseInt(filterElem.id);
+    return filter === ticket.segments[0].stops.length
   });
 }
-// filtersInit(arrTest);
-// Конец
 
 const cheapSort = (arr) => {
-  console.log('cheap sort called');
   for (let i = 1, l = arr.length; i < l; i++) {
       const current = arr[i];
       let j = i;
@@ -232,11 +108,10 @@ const cheapSort = (arr) => {
 };
 
 const fastSort = (arr) => {
-  console.log('fast sort called');
   for (let i = 1, l = arr.length; i < l; i++) {
       const current = arr[i];
       let j = i;
-      while (j > 0 && arr[j - 1]['segments'][0]['duration'] > current['segments'][0]['duration']) {
+      while (j > 0 && (arr[j - 1]['segments'][0]['duration'] + arr[j - 1]['segments'][1]['duration']) > (current['segments'][0]['duration'] + current['segments'][1]['duration'])) {
           arr[j] = arr[j - 1];
           j--;
       }
@@ -246,53 +121,45 @@ const fastSort = (arr) => {
 };
 
 const fetchAndUpdate = async() => {
-  // const sortCheap = document.getElementById('cheap');
-  // const sortFast = document.getElementById('cheap');
-
-  // let cheap = false;
-
-  // sortCheap.addEventListener('change', function() {
-    //   cheap = true;
-    // });
-    // sortFast.addEventListener('change', function() {
-      //   cheap = false;
-      // });
-
-    let ticketsArr = [];
-    let newTicketsArr = [];
-    filters.forEach(element => {
-      element.addEventListener('change', function(){
-        if(element.checked){
-          const filterArr = filtersInit(ticketsArr, element);
-          newTicketsArr = [];
-          sliceArray(filterArr);
-        }
-      })
-    });
-  // const generateArr = async() => {
-  //   const ticketsURL = 'https://front-test.beta.aviasales.ru/tickets?searchId=' + searchID;
-  //   const ticketsResp = await request(ticketsURL);
-  //   ticketsArr = [...ticketsArr, ...ticketsResp.tickets];
-  //   if (cheap) {
-  //     let sortedArr = cheapSort(ticketsArr);
-  //     insertData(sortedArr);
-  //   } else {
-  //     sortedArr = fastSort(ticketsArr);
-  //     insertData(sortedArr);
-  //   }
-  //   let stop = ticketsResp.stop;
-  //   if (stop) {
-  //     return
-  //   }
-  //   return generateArr()
-  // }
+  let ticketsArr = [];
+  let newTicketsArr = [];
 
   const sliceArray = (ticketsArr) => {
-    if (ticketsArr.length >= 5 && !newTicketsArr.length) {
+    if (ticketsArr.length >= 5) {
       newTicketsArr = ticketsArr.slice(0, 5);
       insertData(newTicketsArr);
     }
   }
+
+  filters.forEach(filter => {
+    filter.addEventListener('change', function(){
+      if(filter.checked){
+        const filtersArray = [...document.querySelectorAll('.filters__checkbox')];
+        const checkedFilters = filtersArray.filter(elem => elem.checked);
+        let tickets = [];
+        checkedFilters.forEach(elem => {
+          tickets = [...filtersInit(ticketsArr, elem)];
+        })
+        console.log('array1 ', tickets);
+        tickets = [...filtersInit(tickets, filter)];
+        console.log('array2 ', tickets);
+        sliceArray(tickets);
+      }
+    })
+  });
+// ФИльтрация конец
+
+  const sortCheap = document.getElementById('cheap');
+  const sortFast = document.getElementById('fast');
+
+  let cheap = true;
+
+  // const obj = {
+  //   0: false,
+  //   getTest () {
+  //     return this.test
+  //   }
+  // }
 
   const generateArr = async(searchID) => {
     let stop = false;
@@ -300,15 +167,36 @@ const fetchAndUpdate = async() => {
       const ticketsURL = 'https://front-test.beta.aviasales.ru/tickets?searchId=' + searchID;
       const ticketsResp = await request(ticketsURL);
       ticketsArr = [...ticketsArr, ...ticketsResp.tickets];
-      sliceArray(ticketsArr);
+      // let sortedArr = cheapSort(ticketsArr);
+      // sliceArray(sortedArr);
       stop = ticketsResp.stop;
     }
+    return ticketsArr
   }
 
+
   const response = await request(URL);
-  await generateArr(response.searchId);
-  // const tickets = ticketsResp.tickets;
-  // tickets.length = 5;
+  let data = await generateArr(response.searchId);
+
+  if (cheap) {
+    let sortedArr = cheapSort(data);
+    sliceArray(sortedArr);
+  } else {
+    let sortedArr = fastSort(data);
+    sliceArray(sortedArr);
+  }
+
+  sortCheap.addEventListener('change', function() {
+    cheap = true;
+    let sortedArr = cheapSort(data);
+    sliceArray(sortedArr);
+  });
+
+  sortFast.addEventListener('change', function() {
+    cheap = false;
+    let sortedArr = fastSort(data);
+    sliceArray(sortedArr);
+  });
 }
 
 fetchAndUpdate();
